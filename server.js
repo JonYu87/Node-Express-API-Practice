0d3b518c833c397354c5cb112bcd7d773814e26e
jon <jonathanyusf@gmail.com>
2021-11-19 14:07:00 -0800

    Build logic for getting ingredients and steps for specific recipe

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const fs = require("fs");
 
-app.use(express.json());
+// app.use(express.json());
 
 app.get("/recipes", function (req, res) {
   fs.readFile("./data.json", "utf8", function (err, data) {
@@ -18,8 +18,27 @@ app.get("/recipes", function (req, res) {
   res.status(200);
 });
 
-
+app.get("/recipes/details/:recipeName", function (req, res) {
+  fs.readFile("./data.json", "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+    //get recipe ingredients details from data.json and give status code 200
+    res.send({
+      details: {
+        ingredients: JSON.parse(data).recipes.find(
+          (recipe) => recipe.name === req.params.recipeName
+        ).ingredients,
+      },
+      //number of steps it takes to make the recipe
+      numSteps: JSON.parse(data).recipes.find(
+        (recipe) => recipe.name === req.params.recipeName
+      ).instructions.length,
+    });
+  });
+  res.status(200);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`Live on localhost ${port}!`);
 });
