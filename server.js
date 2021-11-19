fb15be9ee68b3d8aaa8c1c6dcf4cffe460d16717
jon <jonathanyusf@gmail.com>
2021-11-18 18:04:18 -0800

    Finish simple get route for all recipes names

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ const app = express();
 const port = 3000;
 const fs = require("fs");
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/recipes", function (req, res) {
-    fs.readFile("./data.json","utf8", function (err, data) {
-        if (err) {
-            console.log(err);
-        }
-        res.send(JSON.parse(data));
-        console.log(JSON.parse(data));
-    })
+  fs.readFile("./data.json", "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+    //get recipe names from data.json and give status code 200
+    res.send({
+      recipeNames: JSON.parse(data).recipes.map((recipe) => recipe.name),
+    });
+  });
+  res.status(200);
 });
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
