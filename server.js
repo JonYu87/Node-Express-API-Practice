76576ea4f214e5da1d8127b6179d9a4dd395cd4d
jon <jonathanyusf@gmail.com>
2021-11-18 16:53:34 -0800

    Add nodemon for better workflow

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const fs = require("fs");
 
 app.use(express.json())
 
-app.get("/getRecipes", function (req, res) {
+app.get("/recipes", function (req, res) {
     fs.readFile("./data.json","utf8", function (err, data) {
         if (err) {
             console.log(err);
         }
         res.send(JSON.parse(data));
+        console.log(JSON.parse(data));
     })
 });
 
